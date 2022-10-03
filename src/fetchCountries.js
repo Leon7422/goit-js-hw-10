89435a7baf5ec3d99a1f1159148f6db1aee6f315
Leon7422 <y.kurduban98@gmail.com>
2022-10-03 22:54:39 +0300

    created country search

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+export default function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(responce => responce.json())
+    .catch(e => console.log(e));
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import debounce from 'lodash.debounce';
+const debounce = require('lodash.debounce');
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+input.addEventListener('input', debounce(extraxtCountryName, DEBOUNCE_DELAY));
+
+function extraxtCountryName(e) {
+  const searchArea = e.target.value;
+
+  if (searchArea.trim() === '') {
+    return;
+  }
+
+  fetchCountries(searchArea.trim())
+    .then(data => createAllMarkups(data))
+    .catch(err => console.log(err));
+}
+
+function createAllMarkups(data) {
+  if (!data.length) {
+    clearData();
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+    return;
+  } else if (data.length > 10) {
+    clearData();
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  } else if (data.length >= 2 && data.length <= 10) {
+    clearData();
+    createPreviewMarkup(data);
+  } else {
+    clearData();
+    createFullMarkup(data);
+  }
+}
+
+function createPreviewMarkup(data) {
+  const countryPlace = document.querySelector('.country-list');
+  const markup = data
+    .map(country => {
+      return `<li class="country-list-item"><img class ="country-flag" src="${country.flags.svg}" alt=""><span>${country.name.official}</span></li>`;
+    })
+    .join('');
+  countryPlace.innerHTML = markup;
+}
+
+function createFullMarkup(data) {
+  const countryPlace = document.querySelector('.country-info');
+  const markup = data
+    .map(country => {
+      return `<div class="head"><img class ="flag" src="${
+        country.flags.svg
+      }" alt="">
+      <span class="country-name">${country.name.official}</span></div>
+      <div>Capital: <span>${country.capital}</span></div>
+      <div>Population: <span>${country.population} people</span></div>
+      <div>Languages: <span>${Object.values(country.languages).join(
+        ', '
+      )}</span></div>`;
+    })
+    .join('');
+  countryPlace.innerHTML = markup;
+}
+
+function clearData() {
+  const infoData = document.querySelector('.country-info');
+  const listData = document.querySelector('.country-list');
+  infoData.innerHTML = ' ';
+  listData.innerHTML = ' ';
+}
